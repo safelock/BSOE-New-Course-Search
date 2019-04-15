80da656761d0c4dc68af551e27b94dbd81b6e357
Gico Carlo Evangelista <gnevange@ucsc.edu>
2019-04-15 14:33:24 -0700

    added comments

--- src/script/scrape.py
+++ src/script/scrape.py
@@ -2,45 +2,51 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+'''
+Created by: Gico Carlo Evangelista (RiceAbove)
+'''
+
 # Write courses at object literal
-def write_js():
+def write_js(old, new, desc):
     pass
 
-# Will write object literal to js file
+# Scrape BSOE renumbering table
 def scrape():
-    link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSaEqu5y2LqKl6BnV4XNwViiTi5p11ltC9zQPLt0Qb6NHVrWKmfqQ5o3wt5StzR0mtjJck3RW1R3T5w/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false&amp;rm=minimal#s'
-    main = requests.get(link).text
-    soup = BeautifulSoup(main, "lxml")
-    count = 0
     try:
+        link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSaEqu5y2LqKl6BnV4XNwViiTi5p11ltC9zQPLt0Qb6NHVrWKmfqQ5o3wt5StzR0mtjJck3RW1R3T5w/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false&amp;rm=minimal#s'
+        main = requests.get(link).text
+        soup = BeautifulSoup(main, "lxml")
         for courses in soup.find_all("tr"):
             course = courses.find_all('td', class_='s4')
             if len(course) == 3:
                 old_course = course[0].text
                 new_course = course[1].text
                 desc = course[2].text
-                print('{} => {} : {}'.format(old_course, new_course, desc))
+                print('{}   \t{}   \t{}'.format(old_course, new_course, desc))
+                write_js(old_course, new_course, desc)
     except Exception as e:
         print('Error while scraping', e)
 
-def start():
-    print('-----------------------------------------')
+# For testing. Check if script stars
+def start_pompt():
+    print('-------------------------------------------------------------------')
     print('Start Scrape')
-    print('-----------------------------------------')
+    print('-------------------------------------------------------------------')
 
-def end():
-    print('-----------------------------------------')
+# For testing. Check if script ends
+def end_prompt():
+    print('-------------------------------------------------------------------')
     print('End Scrape')
-    print('-----------------------------------------')
-
+    print('-------------------------------------------------------------------')
+    
 # Main function
 def main():
     # f = open('../js/main.js', 'a+')
-    start()
+    start_pompt()
     scrape()
-    end()
+    end_prompt()
     # f.close()
 
-# Start scrape
+# Start script
 if __name__ == '__main__':
     main()
